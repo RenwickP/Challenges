657647ba453b55d038062b4ebf88342af8a54b9d
renwickp <peter2renwick@gmail.com>
2020-03-16 21:13:58 -0600

    Math

--- index.js
+++ index.js
@@ -26,3 +26,9 @@ function filterArray(arr) {
   }
   return x;
 }
+
+// This Triangular Number Sequence is generated from a pattern of dots that form a triangle. The first 5 numbers of the sequence, or dots, are: 1, 3, 6, 10, 15. Write a function that converts n number of places with its corresponding number.
+
+function triangle(n) {
+  return (n * (n + 1)) / 2;
+}
