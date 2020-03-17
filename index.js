558708d3f131372f6a061ce5f083c8033b3bd278
renwickp <peter2renwick@gmail.com>
2020-03-16 21:16:37 -0600

    next

--- index.js
+++ index.js
@@ -32,3 +32,11 @@ function filterArray(arr) {
 function triangle(n) {
   return (n * (n + 1)) / 2;
 }
+
+// Create a function that returns true when num1 is equal to num2; otherwise return false.
+
+function isSameNum(num1, num2) {
+  if (num1 === num2) {
+    return true;
+  } else return false;
+}
